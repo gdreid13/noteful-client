b6405f176c9204aaa014f13c8f9c57dbeb995e57
gdreid13 <gdreid13@gmail.com>
2020-10-06 05:36:29 -0700

    changed endpoint in config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://glacial-ravine-56646.herokuapp.com/api',
+  DEV_API_ENDPOINT: 'http://localhost:8000/api',
   PROD_API_ENDPOINT: 'https://glacial-ravine-56646.herokuapp.com/api',
   API_KEY: process.env.REACT_APP_API_KEY,
 }
